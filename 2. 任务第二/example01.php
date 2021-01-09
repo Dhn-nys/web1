c35bd1abb5ac16b49886606c906bcaca12e1f2a6
Zhue Xia <dhenys.1002@gmail.com>
2021-01-09 13:44:43 +0700

    Example 01
    Example function to HTML

---
+++ 2. 任务第二/example01.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>Contoh 01</title> <!-- Judul Web -->
+ </head>
+ <body>
+ Halo Apa Kabar?<br> <!-- Perintah dibuat di antara <body> dan </body> -->
+ Mudah-Mudahan kabarnya baik
+ </body>
+</html>
