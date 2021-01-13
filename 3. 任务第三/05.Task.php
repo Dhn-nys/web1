eb0bb7dfd2b69d3c353f0f9ddfc7c7716a42f336
デニス <dhenys.1002@gmail.com>
2021-01-13 14:23:35 +0700

    Create 05.Task.php
    Task iam use method define and variable

---
+++ 3. 任务第三/05.Task.php
@@ -0,0 +1,22 @@
+<html>
+ <head>
+  <title>Menghitung Luas Lingkaran</title>
+ </head>
+ <?php
+ 
+    # Method "define"
+	define("nama", "Widi Mawardi");
+	define("Alamat", "Jl Ibu Ganirah No 252 D Cimahi-Bandung");
+	
+	# Method "variabel"
+	$ttl = "Jakarta, 20 Oktober 1990";
+	$no = "0876778778";
+	
+	# Output :3
+	
+	echo "Nama Saya : ", nama;
+	echo "<br />Alamat : ", Alamat;
+	echo"<br />Ttl : $ttl<br />";
+	echo"No Telepon : $no<br />";
+ ?>
+</html>
