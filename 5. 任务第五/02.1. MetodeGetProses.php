59803eb6db201cefbc39846b120a6458dfa1a118
Dhn-nys <dhenys.1002@gmail.com>
2021-01-15 19:04:07 +0700

    Create 02.1. MetodeGetProses.php
    Link -> 02.2. MetodeGetProses.php

---
+++ 5. 任务第五/02.1. MetodeGetProses.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>FORM METODE GET</title>
+ </head>
+ <body>
+  <form action="02.2. MetodeGetProses.php" method="get">
+  Masukkan nama : <input type="text" name="nama" size="25">
+  <input type="submit" value="Proses">
+ </body>
+</html>
