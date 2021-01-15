9e4f5b0a08edbeec55dd097b9a4a52305047868d
Dhn-nys <dhenys.1002@gmail.com>
2021-01-15 19:04:58 +0700

    Create 02.2. MetodeGetProses.php
    Link -> 02.1. MetodeGetProses.php -> view method GET

---
+++ 5. 任务第五/02.2. MetodeGetProses.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>FORM METODE GET</title>
+ </head>
+ <body>
+  Data Nama Yang Diinputkan Adalah : <?php echo $_GET["nama"]; ?>
+  
+  <!-- improvisation -->
+  <br />
+  <a href="02.1. MetodeGetProses.php">Kembali</a>
+ </body>
+</html>
+
