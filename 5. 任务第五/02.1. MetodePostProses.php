bb33e223792f8986684bc94c458ea88c7a3c0896
デニス <dhenys.1002@gmail.com>
2021-01-22 20:21:32 +0700

    Create 02.1. MetodePostProses.php
    Method "Post" form

---
+++ 5. 任务第五/02.1. MetodePostProses.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>FORM METODE POST</title>
+        </head>
+        <body>
+            <form action="02.2. MetodePostProses.php" method="post">
+                Masukkan nama : <input type="text" name="nama" size="25">
+                <input type="submit" value="Proses">
+            </form>
+        </body>
+    </html>
