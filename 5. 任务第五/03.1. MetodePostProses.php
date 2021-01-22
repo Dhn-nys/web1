9b52656513e9dd4f813a9fb6a8c967a63a67b679
デニス <dhenys.1002@gmail.com>
2021-01-22 20:22:34 +0700

    Update and rename 02.1. MetodePostProses.php to 03.1. MetodePostProses.php
    Rename 02.1. MetodePostProses.php to 03.1. MetodePostProses.php

--- 5. 任务第五/03.1. MetodePostProses.php
+++ 5. 任务第五/03.1. MetodePostProses.php
@@ -4,7 +4,7 @@
             <title>FORM METODE POST</title>
         </head>
         <body>
-            <form action="02.2. MetodePostProses.php" method="post">
+            <form action="03.2. MetodePostProses.php" method="post">
                 Masukkan nama : <input type="text" name="nama" size="25">
                 <input type="submit" value="Proses">
             </form>
