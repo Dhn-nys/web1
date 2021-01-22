0e6ab46f2f679ff845f1a53e4acdea6efe5ae136
デニス <dhenys.1002@gmail.com>
2021-01-22 20:26:02 +0700

    Create 03.2. MetodePostProses.php
    View method "Post". look in the bar browser to difference method get, and method post

---
+++ 5. 任务第五/03.2. MetodePostProses.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>METHOD POST PROCESS</title>
+    </head>
+    <body>
+        Data Nama Yang Diinputkan adalah : <?php echo $_POST["nama"];?>
+        <!-- improvisation -->
+        <br />
+        <a href="03.1. MetodePostProses.php">Kembali</a>
+    </body>
+</html>
