b1a5588576580e1369f9bdad486bab3086827e2c
デニス <dhenys.1002@gmail.com>
2021-01-29 19:07:51 +0700

    Update 01.1 FormInputMahasiswa.php
    Add Script

--- 6. 任务第六/01.1 FormInputMahasiswa.php
+++ 6. 任务第六/01.1 FormInputMahasiswa.php
@@ -24,3 +24,14 @@
                     <option value="Wirausaha">Wirausaha</option>
                     <option value="Lain-lain">Lain-lain</option>
                 </select>
+                <p>
+                    Hobi :
+                    <input type="checkbox" name="hobi1" value="Olahraga" />Olahraga
+                    <input type="checkbox" name="hobi2" value="Musik" />Musik
+                    <input type="checkbox" name="hobi3" value="Jalan-Jalan" />Jalan-Jalan
+                <p>
+                    <input type="submit" value="Kirim" />
+                    <input type="reset" value="Batal" />
+                </form>
+    </body>
+</html>
